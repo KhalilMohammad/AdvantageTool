0546f4daef975d0f4bf1424dda8135c658c8cca1
KhalilMohammad <khalilmohammadmirza@gmail.com>
2020-11-17 20:06:25 +0500

    Add tool view

---
+++ AdvantageTool/Controllers/ToolController.cs
@@ -0,0 +1,32 @@
+﻿using AdvantageTool.Lti;
+using AdvantageTool.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
+
+namespace AdvantageTool.Controllers
+{
+    [Authorize]
+    public class ToolController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var idToken = await HttpContext.GetTokenAsync(OpenIdConnectResponseType.IdToken);
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(idToken);
+
+            var response = new ToolResponseModel
+            {
+                IdToken = idToken,
+                LtiRequest = new LtiResourceLinkRequest(jwt.Payload),
+                JwtHeader = jwt.Header,
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ AdvantageTool/Models/ToolResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using AdvantageTool.Lti;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AdvantageTool.Models
+{
+    public class ToolResponseModel
+    {
+        /// <summary>
+        /// The error discovered while parsing the request.
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// A copy of the id_token for diagnostic purposes.
+        /// </summary>
+        public string IdToken { get; set; }
+
+        /// <summary>
+        /// The parsed JWT header from id_token. Null if invalid token.
+        /// </summary>
+        public JwtHeader JwtHeader { get; set; }
+
+        /// <summary>
+        /// Wrapper around the request payload.
+        /// </summary>
+        public LtiResourceLinkRequest LtiRequest { get; set; }
+    }
+}
